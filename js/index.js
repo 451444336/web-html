3de5e077ebf03614523a393d430472a1208a97a4
lb14 <123456>
2018-05-04 11:13:12 +0800

    全房通1

---
+++ js/index.js
@@ -0,0 +1,129 @@
+$(function(){
+    //禁止鼠标右键
+    /*$(document).bind("contextmenu",function(e){
+        return false;
+    });*/
+    //左侧导航
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+        // Variables privadas
+        var links = this.el.find('.link_tit');
+        var submenu = this.el.find('.subMenu').find('a');
+        var openMenu = this.el.find('li').hasClass('openMenu');
+        if(openMenu){
+            $('.openMenu .subMenu').slideToggle();
+        }
+        // Evento
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+        submenu.on('click',function(){
+            $(this).addClass('action').parent('dd').siblings().find('a').removeClass('action');
+            $(this).addClass('action').parents('.navMenu li').siblings().find('a').removeClass('action');
+        });
+    };
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+        $this = $(this);
+        $next = $this.next();
+        $next.slideToggle();
+        if($next.length != 0){
+            $this.parent().toggleClass('openMenu');
+        }
+        if (!e.data.multiple) {
+            $el.find('.subMenu').not($next).slideUp().parent().removeClass('openMenu');
+        }
+    };
+   new Accordion($('#accordion'), false);
+   //监听浏览器分辨率改变菜单显示
+   $(window).on('resize',function(){
+       var width = $(this).width();
+       if(width < 1920){
+           $('.mainBox').addClass('main-shrink');
+           $('.mainContent').animate({
+               left: '70px'
+           },100);
+       }else{
+           $('.mainBox').removeClass('main-shrink');
+           $('.mainContent').animate({
+               left: '128px'
+           },100);
+       }
+   }).resize();
+   //左侧收缩展开
+    $('.leftNav').on('click','.openBtn',function(){
+        if($('.mainBox').hasClass('main-shrink')){
+            $('.mainBox').removeClass('main-shrink');
+            $('.mainContent').animate({
+                left: '128px'
+            },100);
+        }else{
+            $('.mainBox').addClass('main-shrink');
+            $('.mainContent').animate({
+                left: '70px'
+            },100);
+        }
+    });
+    //收缩展开按钮
+    $('.leftNav').hover(function(){
+        $('.openBtn').show();
+    },function(){
+        $('.openBtn').hide();
+    });
+    //项目搜索、经营数据、推广数据展开收缩
+    $('.searchBtnBox').on('click','.seniorBtn',function(){
+        if($('.searchTermList').hasClass('searchTermShow')){
+            $('.searchTermList').removeClass('searchTermShow');
+        }else{
+            $('.searchTermList').addClass('searchTermShow');
+        }
+    });
+    $('.rightContent').on('click','.titOpenBtn',function(){
+        $('.openPublicBox').hide();
+        if($('.titSearchIco').hasClass('icoHide')){
+            $('.titSearchIco').removeClass('icoHide');
+        }
+        if($('.openRicon').hasClass('upIcon')){
+            $('.openRicon').removeClass('upIcon');
+            $('.openRicon').text('展开');
+        }
+        $(this).parents('.rightPublic').find('.titSearchIco').addClass('icoHide');
+        $(this).parents('.rightPublic').find('.openPublicBox').show();
+        $(this).parents('.rightPublic').find('.openRicon').addClass('upIcon').text('折叠');
+    });
+    //管理员弹出列表
+    $('.subHeadMenu').hover(function(){
+        $('.subHeadMenuList').show();
+    },function(){
+        $('.subHeadMenuList').hide();
+    });
+    //table列表操作菜单
+    $('.operationList li:first-child').find('.subOperMenu').show();
+    $('.wareList li').hover(function(){
+        $(this).parents('.operationList').find('.js_operMenu').removeClass('operHover');
+        $(this).parents('.operationList').find('.subOperMenu').hide();
+        $(this).find('.subOperMenu').show();
+        $(this).find('.js_operMenu').addClass('operHover');
+    });
+    //table列表操作按钮
+    $('.wareList').on('click', '.operationBtn', function(event){
+        //取消事件冒泡
+        event.stopPropagation();
+        if($(this).siblings('.operationList').hasClass('hide')){
+            $(this).siblings('.operationList').removeClass('hide');
+        }else{
+            $(this).siblings('.operationList').addClass('hide');
+        }
+        $(this).parents('tr').siblings('tr').find('.operationList').addClass('hide');
+        return false;
+    });
+    //table列表点击空白处隐藏弹出层，下面为滑动消失效果和淡出消失效果。
+    $(document).click(function(event){
+        var _con = $('.operationList');   // 设置目标区域
+        if(!_con.is(event.target) && _con.has(event.target).length === 0){ // Mark 1
+            //$('#divTop').slideUp('slow');   //滑动消失
+            $('.operationList').addClass('hide');          //淡出消失
+        }
+    });
+    //table 隔行变色
+    $(".wareList tr:nth-child(even)").addClass("trBack");
+});
---
+++ js/modules/tabnav.js
@@ -0,0 +1,49 @@
+/** index.js By Beginner Emain:zheng_jinfan@126.com HomePage:http://www.zhengjinfan.cn */
+
+var tab;
+
+layui.config({
+	base: 'js/modules/',
+	version:new Date().getTime()
+}).use(['layer', 'tab'], function() {
+	var tab = layui.tab({
+			elem: '.admin-nav-card', //设置选项卡容器
+			maxSetting: {
+				max: 6,
+				tipMsg: '只能开5个哇，不能再开了。'
+			},
+			contextMenu:true
+		});
+    //iframe自适应
+    $(window).on('resize', function() {
+        var $content = $('.admin-nav-card .layui-tab-content');
+        $content.height($(this).height() - 80);
+        $content.find('.layui-tab-item').each(function() {
+            $(this).height($content.height());
+        });
+    }).resize();
+	$('#accordion').find('dd').each(function() {
+		var tabTit = $('.layui-tab-title');
+		var tabCon = $('.layui-tab-content');
+		$(this).on('click', function() {
+			var $a = $(this).children('a');
+			var href = $a.data('url');
+			var title = $a.text();
+			var data = {
+				elem: $a,
+				field: {
+					href: href,
+					title: title
+				}
+			};
+            if(tabTit.find('li').length == 6 && tabCon.find('.layui-tab-item').length == 6){
+                var liTxt = tabTit.find('li span').text();
+                if (liTxt.indexOf(title) > 0){}else{
+                    tabTit.find('li').eq(1).remove();
+                    tabCon.find('.layui-tab-item').eq(1).remove();
+                }
+            }
+            tab.tabAdd(data.field);
+		});
+	});
+});
